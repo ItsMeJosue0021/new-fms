ed4c48d33584495d66cd01086309fca9dca9c69f
Joshua Salceda <joshuasalceda0021@gmail.com>
2024-04-24 06:38:29 +0800

    changes

--- resources/views/requests/completed.blade.php
+++ resources/views/requests/completed.blade.php
@@ -22,7 +22,7 @@
                         </form>
                     </div>
                     <div class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
-                        <a href="{{}}" class="ml-2 text-gray-700 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 font-medium rounded-md text-sm px-5 py-2 border border-gray-300">Print</a>
+                        <a class="ml-2 text-gray-700 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 font-medium rounded-md text-sm px-5 py-2 border border-gray-300">Print</a>
                     </div>
                 </div>
                 <div class="overflow-x-auto">
