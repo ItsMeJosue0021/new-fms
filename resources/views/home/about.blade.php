22c7f8dba5bb0f71126ff032bcfa0d45a0f0cff5
Joshua Salceda <joshuasalceda0021@gmail.com>
2024-04-24 05:52:21 +0800

    Added code for about page

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -1,169 +1,51 @@
 <x-guest-layout>
     <div>
-        <section class="w-full max-w-[1400px] mx-auto px-4">
-            <div class="py-12 ">
-                <div class="w-full ">
-
-                    <div class="lg:text-center">
-                        <h2
-                            class="font-heading mb-4 bg-orange-100 text-orange-800 px-4 py-2 rounded-lg md:w-64 md:mx-auto text-xs font-semibold tracking-widest  uppercase title-font">
-                            Why choose us?
-                        </h2>
-                        <p class="font-heading mt-2 text-3xl leading-8 font-semibold tracking-tight text-gray-900 sm:text-4xl">
-                            We know tech, we know finance. We are fintech experts.
-                        </p>
-                        <p class="mt-4 max-w-2xl text-lg text-gray-500 lg:mx-auto">
-                            We know how to handle taxation for all the
-                            countried we operate in. We care for our customers and help them manage cashflows.
-                        </p>
-                    </div>
-
-                    <div class="mt-10">
-                        <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-                            <div class="relative">
-                                <dt>
-                                    <div
-                                        class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
-                                        <img src="https://www.svgrepo.com/show/503163/api-settings.svg" >
-                                    </div>
-                                    <p class="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">Powerful API</p>
-                                </dt>
-                                <dd class="mt-2 ml-16 text-base text-gray-500">
-                                    Lorem ipsum, dolor sit amet consectetur
-                                    adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
-                                    blanditiis ratione.
-                                </dd>
-                            </div>
-                            <div class="relative">
-                                <dt>
-                                    <div
-                                        class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
-                                        <img src="https://www.svgrepo.com/show/503138/webpack.svg" >
-                                    </div>
-                                    <p class="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">Easy to integrate
-                                        SDK
-                                    </p>
-                                </dt>
-                                <dd class="mt-2 ml-16 text-base text-gray-500"> Lorem ipsum, dolor sit amet consectetur
-                                    adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
-                                    blanditiis ratione.
-                                </dd>
-                            </div>
-                            <div class="relative">
-                                <dt>
-                                    <div
-                                        class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
-                                        <img src="https://www.svgrepo.com/show/511771/dashboard-671.svg" >
-
-                                    </div>
-                                    <p class="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">Low Transaction Cost
-                                    </p>
-                                </dt>
-                                <dd class="mt-2 ml-16 text-base text-gray-500"> Lorem ipsum, dolor sit amet consectetur
-                                    adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
-                                    blanditiis ratione.
-                                </dd>
-                            </div>
-                            <div class="relative">
-                                <dt>
-                                    <div
-                                        class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
-                                        <img src="https://www.svgrepo.com/show/76267/free-commercial-label.svg" >
-
-                                    </div>
-                                    <p class="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">Powerful Dashboard
-                                    </p>
-                                </dt>
-                                <dd class="mt-2 ml-16 text-base text-gray-500"> Lorem ipsum, dolor sit amet consectetur
-                                    adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
-                                    blanditiis ratione.
-                                </dd>
-                            </div>
-                        </dl>
-                    </div>
-
-                </div>
+        <section class="text-black">
+            <div class="flex justify-center">
+              <h1 class="text-4xl font-bold uppercase mb-4">
+                About Us
+              </h1>
             </div>
-
-            <ol class="items-center sm:flex">
-                <li class="relative mb-6 sm:mb-0">
-                    <div class="flex items-center">
-                        <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                            <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                            </svg>
-                        </div>
-                        <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+            <div class="container max-w-5xl px-4 py-12 mx-auto">
+              <div class="grid gap-4 mx-4 sm:grid-cols-12">
+                <div class="col-span-12 sm:col-span-3">
+                  <div class="text-center sm:text-left mb-14 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:bg-blue-800">
+                    <h3 class="text-3xl font-semibold">Our Story</h3>
+                    <span class="text-sm font-bold tracki uppercase text-gray-600">Torres Escaro Funeral</span>
+                  </div>
+                </div>
+                <div class="relative col-span-12 px-4 space-y-6 sm:col-span-9">
+                  <div class="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-gray-300">
+                    <div class="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-blue-800">
+                      <h3 class="text-xl font-semibold tracki">Torres Escaro Funeral services</h3>
+                      <time class="text-xs tracki uppercase text-gray-600">2009</time>
+                      <p class="mt-3">Our funeral business is established and begins its operations.</p>
                     </div>
-                    <div class="mt-3 sm:pe-8">
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.0.0</h3>
-                        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on December 2, 2021</time>
-                        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
+                    <div class="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-blue-800">
+                      <h3 class="text-xl font-semibold tracki">Initial Years</h3>
+                      <time class="text-xs tracki uppercase text-gray-600">2009</time>
+                      <p class="mt-3">Our business starts by offering hearse and van rental services primarily for large funerals within the metro area. It begins with only 4 employees.</p>
                     </div>
-                </li>
-                <li class="relative mb-6 sm:mb-0">
-                    <div class="flex items-center">
-                        <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                            <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                            </svg>
-                        </div>
-                        <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                    <div class="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-blue-800">
+                      <h3 class="text-xl font-semibold tracki">Continued Growth</h3>
+                      <time class="text-xs tracki uppercase text-gray-600">2009</time>
+                      <p class="mt-3">Over the years, the funeral business experiences continuous growth and expansion.</p>
                     </div>
-                    <div class="mt-3 sm:pe-8">
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.2.0</h3>
-                        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on December 23, 2021</time>
-                        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
+                    <div class="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-blue-800">
+                      <h3 class="text-xl font-semibold tracki">Services Diversification</h3>
+                      <time class="text-xs tracki uppercase text-gray-600">2009</time>
+                      <p class="mt-3">The funeral business expands its services to include memorial services, cremation services, as well as transportation and repatriation services.</p>
                     </div>
-                </li>
-                <li class="relative mb-6 sm:mb-0">
-                    <div class="flex items-center">
-                        <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                            <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                            </svg>
-                        </div>
-                        <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                    <div class="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-blue-800">
+                      <h3 class="text-xl font-semibold tracki">Client Base</h3>
+                      <time class="text-xs tracki uppercase text-gray-600">The funeral business caters to both local and international clients</time>
+                      <p class="mt-3">Local Clients: Most local clients avail of the memorial services.</p>
+                      <p class="mt-3">International Clients: International clients often utilize the transportation and repatriation services.</p>
                     </div>
-                    <div class="mt-3 sm:pe-8">
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.3.0</h3>
-                        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on January 5, 2022</time>
-                        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
-                    </div>
-                </li>
-                <li class="relative mb-6 sm:mb-0">
-                    <div class="flex items-center">
-                        <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                            <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                            </svg>
-                        </div>
-                        <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-                    </div>
-                    <div class="mt-3 sm:pe-8">
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.3.0</h3>
-                        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on January 5, 2022</time>
-                        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
-                    </div>
-                </li>
-                <li class="relative mb-6 sm:mb-0">
-                    <div class="flex items-center">
-                        <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                            <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                            </svg>
-                        </div>
-                        <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-                    </div>
-                    <div class="mt-3 sm:pe-8">
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.3.0</h3>
-                        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on January 5, 2022</time>
-                        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
-                    </div>
-                </li>
-            </ol>
-
-
-        </section>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
     </div>
 </x-guest-layout>
